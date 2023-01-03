69db768cf72f8e14ebc5b8b5903352937858050a
Christina-Westar <121840467+Christina-Westar@users.noreply.github.com>
2023-01-03 14:32:03 +0800

    test axios

---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>1234</h1>
+</template>
+<script>
+// import { defineComponent } from '@vue/composition-api'
+import { axios } from 'axios';
+
+export default {
+    setup() {
+        
+    },
+}
+</script>
